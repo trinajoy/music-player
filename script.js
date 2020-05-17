3431e2646aa694b83943b11a4f1b72ad5453d34c
Katrina <katrinahitchcock@Katrinas-MacBook-Air.local>
2020-05-17 11:29:36 +0200

    update songs and images

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const title = document.getElementById('title');
 const cover = document.getElementById('cover');
 
 // Song titles
-const songs = ['hey', 'summer', 'ukulele'];
+const songs = ['kylie', 'inxs', 'veronicas'];
 
 // Keep track of song
 let songIndex = 2;
